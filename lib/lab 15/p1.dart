54eb9ec457154f7548adfc5a35f1e2160667f81f
Tirth1107 <24020201071@darshan.ac.in>
2026-07-29 12:14:09 +0530

    fix -3

---
+++ lib/lab 15/p1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class p1 extends StatefulWidget {
+  const p1({super.key});
+
+  @override
+  State<p1> createState() => _p1State();
+}
+
+class _p1State extends State<p1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Practical 15"),
+        
+      ),
+    );
+  }
+}
