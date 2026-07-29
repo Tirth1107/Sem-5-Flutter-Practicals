5897ec2d97963ff3ff2f8fb297bbfcf819c76820
Tirth1107 <24020201071@darshan.ac.in>
2026-07-29 12:33:36 +0530

    Practical 14

---
+++ lib/lab 15/p2.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class p2 extends StatefulWidget {
+  const p2({super.key});
+
+  @override
+  State<p2> createState() => _p2State();
+}
+
+class _p2State extends State<p2> {
+  String page_text = "Home Page";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Navigation Drawer"),),
+      drawer: NavigationDrawer(
+        children: [
+          DrawerHeader(child: CircleAvatar(child: Icon(Icons.person),)),
+          ListTile(title: Text("Home"),)
+      ]),
+    );
+  }
+}
