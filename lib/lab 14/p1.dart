48f491781fe3bf15fc1db3a0cea2f04f97686c12
Tirth1107 <24020201071@darshan.ac.in>
2026-07-29 12:00:41 +0530

    fix -3

--- lib/lab 14/p1.dart
+++ lib/lab 14/p1.dart
@@ -5,10 +5,23 @@ class p1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: ,),
+      appBar: AppBar(title: Text("Practical 14")),
+      body: PageView(
+        controller: PageController(),
+        children: [
+          buildCard(card_text: "This is page 1"),
+          buildCard(card_text: "This is page 2"),
+          buildCard(card_text: "This is page 3"),
+          buildCard(card_text: "This is page 4"),
+          buildCard(card_text: "This is page 5"),
+        ],
+      ),
     );
   }
 }
 Widget buildCard({required String card_text,Color card_color = Colors.grey}){
-  
+  return Card(
+    elevation: 5,
+    child: Center(child: Text(card_text,style: TextStyle(fontWeight: FontWeight(10),fontSize: 50),),),
+  );
 }
