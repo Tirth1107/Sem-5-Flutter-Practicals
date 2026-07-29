d1bb3b149096c89d9cccc57196cbc5bb3b6e0106
Tirth1107 <24020201071@darshan.ac.in>
2026-07-29 12:36:52 +0530

    Practical 14

--- lib/lab 15/p2.dart
+++ lib/lab 15/p2.dart
@@ -15,7 +15,27 @@ class _p2State extends State<p2> {
       drawer: NavigationDrawer(
         children: [
           DrawerHeader(child: CircleAvatar(child: Icon(Icons.person),)),
-          ListTile(title: Text("Home"),)
+          ListTile(title: Text("Home"),onTap: () {
+            setState(() {
+              page_text = "Home Page";
+            });
+          },),
+          ListTile(title: Text("About"),onTap: () {
+            setState(() {
+              page_text = "About US";
+            });
+          },),
+          ListTile(title: Text("Contact"),onTap: () {
+            setState(() {
+              page_text = "Contact US";
+            });
+          },),
+          ListTile(title: Text("Settings"),onTap: () {
+            setState(() {
+              page_text = "Settings";
+            });
+          },),
+
       ]),
     );
   }
