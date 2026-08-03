cd3f77306da5aff589d1604953db7e4e3c71b589
Tirth1107 <24020201071@darshan.ac.in>
2026-08-03 14:56:17 +0530

    Lab 16 -2

--- lib/lab 16/page1.dart
+++ lib/lab 16/page1.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sem5practicals/lab%2016/page2.dart';
+class page1 extends StatefulWidget {
+  const page1({super.key});
 
+  @override
+  State<page1> createState() => _page1State();
+}
+
+class _page1State extends State<page1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TextButton(onPressed: () => Navigator.push(context,MaterialPageRoute(builder: (context) => page2(),)), child: Text("Go to other page")),
+    );
+  }
+}
--- lib/lab 16/page2.dart
+++ lib/lab 16/page2.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/material.dart';
+class page2 extends StatelessWidget {
+  const page2({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
